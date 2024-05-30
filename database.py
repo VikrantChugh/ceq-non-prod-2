027c24c761d378ab5e25a41c3ff27ce975ef7977
Vikrant <124357732+VikrantChugh@users.noreply.github.com>
2024-05-30 11:41:08 +0530

    Update database.py

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ from details import logger
 def get_secret_from_vault():
     # Initialize the signer using Instance Principals
     signer = signers.InstancePrincipalsSecurityTokenSigner()
-    compartment_id = 'ocid1.compartment.oc1..aaaaaaaa7nxivmvn7wff2j4azbwncx4ywnmfuhx4eugo55huwwuozxysdw4a'
+    # compartment_id = 'ocid1.compartment.oc1..aaaaaaaa7nxivmvn7wff2j4azbwncx4ywnmfuhx4eugo55huwwuozxysdw4a'
     # Initialize the VaultClient with the signer
     vault_client = oci.secrets.SecretsClient({}, signer=signer)
     
@@ -15,7 +15,7 @@ def get_secret_from_vault():
    
     # Set the compartment OCID where the secret is stored
     
-    l=[]
+    secrets=[]
     # Set the secret OCID
     # secret_id = 'ocid1.vaultsecret.oc1.ap-mumbai-1.amaaaaaabfgevmaa43hybdoxsdmbyvzma2stffounarrvh4ytquye26bxtcq'
     db_host1='ocid1.vaultsecret.oc1.ap-mumbai-1.amaaaaaabfgevmaa3nuaiztjxtcs3l4ewmjuwwdwrsi7rzohb2qb5ft4twoa'
@@ -28,25 +28,25 @@ def get_secret_from_vault():
     secret_id_list=[db_host1,db_pass1,db_name1,db_user1,snow_password,snow_username]
 
     try:
-        for m in secret_id_list:
+        for secret_id in secret_id_list:
             # print(m)
         # Retrieve the secret from the Vault
-            secret_bundle = vault_client.get_secret_bundle(m).data
+            secret_bundle = vault_client.get_secret_bundle(secret_id).data
 
             # Extract the secret value
             db_pass = secret_bundle.secret_bundle_content.content
             password=base64_to_plain_text(db_pass)
             # print(password)
-            l.append(password)
+            secrets.append(password)
 
         # print(l)  
         details={
-            'db_host':l[0],
-            'db_pass':l[1],
-            'db_name':l[2],
-            'db_user':l[3],
-            'snow_api_password':l[4],
-            'snow_api_username':l[5]
+            'db_host':secrets[0],
+            'db_pass':secrets[1],
+            'db_name':secrets[2],
+            'db_user':secrets[3],
+            'snow_api_password':secrets[4],
+            'snow_api_username':secrets[5]
 
 
         }
